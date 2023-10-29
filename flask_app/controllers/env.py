718b077a1301240608781b50a09b56ffca168aa1
Serxhio99 <114481413+Serxhio99@users.noreply.github.com>
2023-10-29 21:34:44 +0100

    Update env.py

--- flask_app/controllers/env.py
+++ flask_app/controllers/env.py
@@ -2,5 +2,5 @@
 UPLOAD_FOLDER = 'flask_app/static/UPLOADED_FOLDER'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'mp4'}
 
-ADMINEMAIL = ""
-PASSWORD = ""
+ADMINEMAIL = "your email"
+PASSWORD = "password from your mail ap"
