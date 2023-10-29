b677efd4c5779985f4b535bd78704c4a4219863e
Serxhio99 <114481413+Serxhio99@users.noreply.github.com>
2023-10-29 21:36:56 +0100

    mysqlconnection.py

--- flask_app/config/mysqlconnection.py
+++ flask_app/config/mysqlconnection.py
@@ -4,8 +4,8 @@ import pymysql.cursors
 class MySQLConnection:
     def __init__(self, db):
         connection = pymysql.connect(host='localhost',
-                                     user='root',
-                                     password='Password1?',
+                                     user='database_user',
+                                     password='database_user_password',
                                      db=db,
                                      charset='utf8mb4',
                                      cursorclass=pymysql.cursors.DictCursor,
